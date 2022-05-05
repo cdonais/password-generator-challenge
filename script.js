4bf106b3e08f6c65614facff1bd2bffe111d0f2e
Carly Donais <carly.donais@gmail.com>
2022-05-05 18:07:42 -0400

    added character arrays

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 // Assignment code here 
+var lowerCaseArr=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var upperCaseArr=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var numberArr=['1','2','3','4','5','6','7','8','9','0'] 
+var specialCharArr=['!','@','#','$','%','^','&','*','(',')','-','+','=']
+
 function generatePassword () {
     // prompt user for password length
-    var lengthEl=window.prompt("How long would you like your password to be? Please choose a number between 8 and 128.");
-    if (lengthEl === "" || lengthEl === null) {
-        window.alert("Please provide a valid answer.")    
+    const lengthEl=Number(window.prompt("How long would you like your password to be? Choose between 8-128 characters."))
+    if (lengthEl < 8 || lengthEl > 128) {
+        window.alert("Please enter a number over 8 and below 128.")
     }
+
     // prompt user for lowercase letters
-    var lowercaseEl=window.prompt("Would you like to use lowercase letters? Please answer 'YES' or 'NO'");
-    if (lowercaseEl === YES || lowercaseEl === yes) {
-        
+    var addLower=window.prompt("Would you like to use lowercase letters?");
+    if (addLower === YES || addLower === yes) {
+         for (let i=1; i<=lengthEl; i++) {
+            let password=(Math.floor(Math.random) * lengthEl)
+         }
     }
+    //if yes, include in password
     
     // prompt user for uppercase letters
-    var uppercaseEl=window.prompt("Would you like to use uppercase letters? Please answe 'YES' or 'NO'");
-    if (uppercaseEl === YES || uppercaseEl === yes){
+    var addUpper=window.prompt("Would you like to use uppercase letters?");
+    if (addUpper === YES || addUpper === yes) {
+        for (let i=1; i<lengthEl; i++) {
+            let password=(Math.floor(Math.random)*lengthEl)
+        }
 
     }
+    //if yes, include in password
+    
     //prompt for numbers
-    var numberEl=window.prompt("Would you like to use numbers? Please answer 'YES' or 'NO'");
-    if (numberEl === YES || numberEl === yes) {
-
+    var addNum=window.prompt("Would you like to use numbers?");
+    if (addNum === YES || addNum === yes){
+        let password=(Math.floor(Math.random)* lengthEL)
     }
-    // prompt user for special characters
-    var specialEl=window.prompt("Would you like to use special characters? Please answer 'YES' or 'NO'");
-    if (specialEl === YES || specialEl === yes){
-
+    //if yes, include in password
+    
+    //prompt for special characters
+    var addSpec=window.prompt("Would you like to use special characters?");
+    if (addSpec === YES || addSpec === yes) {
+        let password=(Math.floor(Math.random)* lengthEl)
     }
+    //if yes, include in password
+    
     // create password based on selected criteria
+    
     // generate password
-// Get references to the #generate element
+
+    // Get references to the #generate element
     return value;
 }
 var generateBtn = document.querySelector("#generate");
